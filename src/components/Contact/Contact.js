7a4500dcdf8b37bbd2dc703f20226a264901b02c
Gabriel Duarte <gabrielqduarte@gmail.com>
2020-03-02 00:35:55 +0100

    change form provider to netlify forms
    seems more structured

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { useForm, useField } from 'react-final-form-hooks'
 import styled from 'styled-components/macro'
-import ky from 'ky'
 
 import Field from '../Field'
 import Button from '../Button/Button'
@@ -14,107 +12,43 @@ const ButtonContainer = styled.div`
   margin: 4px;
 `
 
-const ThankYou = styled.div`
-  text-align: center;
-  margin: 8px 0;
-`
-
-const isFilled = (value) => {
-  return value !== null && value !== undefined && value !== ''
-}
-
-const createFormData = (values) => ({
-  ...values,
-  _origin: location.origin,
-  _form: `Contact on ${location.origin}`,
-})
-
 const Contact = () => {
-  const [submitted, setSubmitted] = React.useState(false)
+  const [submitting, setSubmitting] = React.useState(false)
 
-  const onSubmit = async (values) => {
-    const form = createFormData(values)
-
-    await ky.post('https://formcarry.com/s/ypblsNbpZ95c', {
-      json: form,
-      headers: {
-        Accept: 'application/json',
-      },
-    })
-
-    setSubmitted(true)
+  // When doing SSR, location does not exist, so we make this check
+  let origin
+  if (typeof location !== `undefined`) {
+    origin = location.origin
   }
-
-  const validate = (values) => {
-    const errors = {}
-    const emailRegex = /(.+)@(.+).(.+){2,}/
-    if (!emailRegex.test(values.email)) {
-      errors.email = 'Invalid email'
-    }
-
-    const requiredFields = ['email', 'name', 'subject', 'message']
-    requiredFields.forEach((field) => {
-      if (!isFilled(values[field])) {
-        errors[field] = 'Required field'
-      }
-    })
-
-    return errors
-  }
-
-  const { form, handleSubmit, submitting } = useForm({
-    onSubmit,
-    validate,
-  })
-  const email = useField('email', form)
-  const name = useField('name', form)
-  const subject = useField('subject', form)
-  const message = useField('message', form)
-
-  // eslint-disable-next-line consistent-return
-  React.useEffect(() => {
-    if (submitted) {
-      const timeout = setTimeout(() => setSubmitted(false), 5000)
-
-      return () => clearTimeout(timeout)
-    }
-  }, [submitted])
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      name="contact"
+      method="POST"
+      data-netlify
+      data-netlify-honeypot="ravena"
+    >
       <Title>Contact me</Title>
 
-      <Field
-        type="email"
-        label="E-mail"
-        error={email.meta.touched && email.meta.error}
-        {...email.input}
-      />
-      <Field
-        label="Name"
-        error={name.meta.touched && name.meta.error}
-        {...name.input}
-      />
-      <Field
-        label="Subject"
-        error={subject.meta.touched && subject.meta.error}
-        {...subject.input}
-      />
-      <Field
-        label="Message"
-        textarea
-        error={message.meta.touched && message.meta.error}
-        {...message.input}
-      />
+      <Field name="email" type="email" label="E-mail" required />
+      <Field name="name" label="Name" required />
+      <Field name="subject" label="Subject" required />
+      <Field name="message" label="Message" textarea required />
+      <input type="hidden" name="_origin" value={origin} />
+      <div hidden>
+        <input name="ravena" />
+      </div>
 
-      {submitted && (
-        <ThankYou>
-          Thank you for contacting me! I'll be replying as soon as possible.
-        </ThankYou>
-      )}
+      {/* The `form-name` hidden field is required to support form submissions without JavaScript on netlify forms */}
+      <input type="hidden" name="form-name" value="contact" />
 
       <ButtonContainer>
-        <Button submitting={submitting} type="submit" value="Send" />
+        <Button
+          onClick={() => setSubmitting(true)}
+          submitting={submitting}
+          type="submit"
+          value="Send"
+        />
       </ButtonContainer>
     </form>
   )
