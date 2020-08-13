5d9b33c5148f2362226618af4a7490e7238929cb
Gabriel Duarte <gabrielqduarte@gmail.com>
2020-08-14 00:32:41 +0200

    add process as an eslint global

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ const baseConfig = require('gd-configs/eslint/web')
 
 module.exports = {
   ...baseConfig,
+  globals: {
+    process: true,
+  },
   rules: {
     ...baseConfig.rules,
     'react/no-unescaped-entities': 'off',
