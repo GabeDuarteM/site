2bc752bbf2ccb85b68e8a05348be5dcae94004a4
Gabriel Duarte <gabrielqduarte@gmail.com>
2023-03-26 03:34:44 +0000

    add some missing files

---
+++ README.md
@@ -0,0 +1,3 @@
+# site
+
+<https://www.gabrielduarte.dev>
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -4,4 +4,12 @@ module.exports = {
   trailingComma: 'all',
   printWidth: 120,
   plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
 }
